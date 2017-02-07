b580524b8f8376423f4857a62adbbcb29d58d483
Miguel Roman <miguelerm@gmail.com>
2017-02-07 13:15:06 -0600

    primeras pruebas con xterm.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,27 @@
+var windowHeight = window.innerHeight;
 var container = document.getElementById('terminal-container');
-var terminal = new Terminal();
+container.style.height = windowHeight + 'px';
+
+window.onresize = function () {
+    windowHeight = window.innerHeight;
+    container.style.height = windowHeight + 'px';
+    terminal.fit();
+}
+
+var terminal = new Terminal({cursorBlink: true});
 terminal.open(container);
+
 terminal._initialized = true;
 var shellprompt = '$ ';
 terminal.prompt = function () {
     terminal.write('\r\n' + shellprompt);
 };
-terminal.writeln('Welcome to xterminal.js');
-terminal.writeln('This is a local terminal emulation, without a real terminal in the back-end.');
-terminal.writeln('Type some keys and commands to play around.');
-terminal.writeln('');
+terminal.writeln('Welcome to Terminator');
+terminal.writeln('Type help to get more information.');
 terminal.prompt();
+
+terminal.toggleFullscreen();
+terminal.fit();
 
 terminal.on('key', function (key, ev) {
     var printable = (!ev.altKey && !ev.altGraphKey && !ev.ctrlKey && !ev.metaKey);
@@ -30,3 +41,16 @@ terminal.on('key', function (key, ev) {
 terminal.on('paste', function (data, ev) {
     terminal.write(data);
 });
+
+//var initialGeometry = terminal.proposeGeometry();
+//var rows = initialGeometry.rows;
+var charHeight = terminal.viewport.currentRowHeight;
+var rows = parseInt(windowHeight / charHeight);
+
+terminal.focus();
+
+/*
+
+var height = (rows * charHeight).toString() + 'px';
+container.style.height = height;
+*/
