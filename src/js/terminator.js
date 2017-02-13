134eebd5fe96299a74a2e198cce238399b843cc4
Miguel Roman <miguelerm@gmail.com>
2017-02-12 20:24:27 -0600

    primer intento de capturar eventos keyperessed

--- src/js/terminator.js
+++ src/js/terminator.js
@@ -32,6 +32,21 @@ document.onreadystatechange = function () {
     }
 };
 
-document.onkeydown = function () {
-    console.log(arguments);
+var linea = document.getElementById("linea");
+document.onkeydown = function (e) {
+
+    var text = linea.innerText;
+    var key = e.key;
+
+    if (key === "Backspace") {
+        if (text.length > 0) {
+            text = text.substring(0, text.length - 1);
+        }
+    } else {
+        text += key;
+    }
+
+    linea.innerText = text;
+
+    console.log(e.key, e);
 }
