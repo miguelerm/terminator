4711b59eb5e7d030d7a22b546415c372d9ed803f
Miguel Roman <miguelerm@gmail.com>
2017-02-12 20:51:48 -0600

    crear nuevas lineas en la termina.

--- src/js/terminator.js
+++ src/js/terminator.js
@@ -32,21 +32,41 @@ document.onreadystatechange = function () {
     }
 };
 
-var linea = document.getElementById("linea");
+var terminal = document.getElementById("terminal");
+var linea = terminal.getElementsByClassName("linea")[0];
+var comando = linea.getElementsByClassName("comando")[0];
+var cursor = linea.getElementsByClassName("cursor")[0];
+
 document.onkeydown = function (e) {
 
-    var text = linea.innerText;
+    var text = comando.innerText;
     var key = e.key;
 
     if (key === "Backspace") {
         if (text.length > 0) {
             text = text.substring(0, text.length - 1);
         }
-    } else {
+    } else if (key === "Enter") {
+        let div = document.createElement("div");
+        let p = document.createElement("span");
+        div.className = "linea";
+        p.className = "prompt";
+        p.innerHTML = "&gt;";
+        div.appendChild(p);
+        let c = document.createElement("span");
+        c.className = "comando";
+        div.appendChild(c);
+        linea.removeChild(cursor);
+        div.appendChild(cursor);
+        terminal.appendChild(div);
+        linea = div;
+        comando = c;
+        return;
+    } else if (key.length === 1) {
         text += key;
     }
 
-    linea.innerText = text;
+    comando.innerText = text;
 
     console.log(e.key, e);
 }
