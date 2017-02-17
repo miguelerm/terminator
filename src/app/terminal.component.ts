7fa5bccdcf6505aa30c3999cbe30dd82c4b7d4f6
Miguel Roman <miguelerm@gmail.com>
2017-02-17 14:22:49 -0600

    terminal partially migrated to Angular 2

--- src/app/terminal.component.ts
+++ src/app/terminal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, HostListener } from '@angular/core';
 
 @Component({
     selector: 'terminal',
@@ -10,45 +10,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TerminalComponent implements OnInit {
 
-    ngOnInit() {
-        var terminal = document.getElementById("terminal");
-        var linea = terminal.getElementsByClassName("linea")[0];
-        var comando = linea.getElementsByClassName("comando")[0] as HTMLElement;
-        var cursor = linea.getElementsByClassName("cursor")[0];
+    private terminalHost: Element;
+    private terminal: Element;
+    private linea: Element;
+    private comando: HTMLElement;
+    private cursor: Element;
 
-        document.onkeydown = function (e) {
+    constructor(private terminalRef: ElementRef) {
+        this.terminalHost = terminalRef.nativeElement;
+    }
 
-            var text = comando.innerText;
-            var key = e.key;
+    @HostListener('document:keydown', ['$event'])
+    onKeyDown(event: KeyboardEvent) {
+        var text = this.comando.innerText;
+        var key = event.key;
 
-            if (key === "Backspace") {
-                if (text.length > 0) {
-                    text = text.substring(0, text.length - 1);
-                }
-            } else if (key === "Enter") {
-                let div = document.createElement("div");
-                let p = document.createElement("span");
-                div.className = "linea";
-                p.className = "prompt";
-                p.innerHTML = "&gt;";
-                div.appendChild(p);
-                let c = document.createElement("span");
-                c.className = "comando";
-                div.appendChild(c);
-                linea.removeChild(cursor);
-                div.appendChild(cursor);
-                terminal.appendChild(div);
-                linea = div;
-                comando = c;
-                return;
-            } else if (key.length === 1) {
-                text += key;
+        if (key === "Backspace") {
+            if (text.length > 0) {
+                text = text.substring(0, text.length - 1);
             }
-
-            comando.innerText = text;
-
-            console.log(e.key, e);
+        } else if (key === "Enter") {
+            let div = document.createElement("div");
+            let p = document.createElement("span");
+            div.className = "linea";
+            p.className = "prompt";
+            p.innerHTML = "&gt;";
+            div.appendChild(p);
+            let c = document.createElement("span");
+            c.className = "comando";
+            div.appendChild(c);
+            this.linea.removeChild(this.cursor);
+            div.appendChild(this.cursor);
+            this.terminal.appendChild(div);
+            this.linea = div;
+            this.comando = c;
+            console.log("last command: ", text);
+            return;
+        } else if (key.length === 1) {
+            text += key;
         }
+
+        this.comando.innerText = text;
+        //console.log('> ', event.key);
+    }
+
+    ngOnInit() {
+        this.terminal = this.terminalHost.firstElementChild;
+        this.linea = this.terminal.getElementsByClassName("linea")[0];
+        this.comando = this.linea.getElementsByClassName("comando")[0] as HTMLElement;
+        this.cursor = this.linea.getElementsByClassName("cursor")[0];
     }
 
 }
